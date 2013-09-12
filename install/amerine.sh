e02ea2cb1f785b243f336cbfa5ff9d4154aef207
Mark Turner <mark@amerine.net>
2013-09-12 01:18:39 -0700

    Starting to move dotfiles over to this repo.
    Major inspiration/thanks to @mattly for publishing his dotfiles. I borrow
    heavily from the state of his dotfiles (git@github.com:mattly/dotfiles.git) @
    2eca9a4bd3.

---
+++ install/amerine.sh
@@ -0,0 +1,15 @@
+# heavy inspiration from github.com/mattly here.
+
+## Basics
+brew readline
+brew bcrypt
+brew openssl
+
+## Git
+brew git
+brew hub
+brew tig
+
+## Files
+code=$HOME/code/amerine
+github amerine/dotfiles $code/dotfiles
